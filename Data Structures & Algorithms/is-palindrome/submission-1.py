96f65b91cc0a420f652f71b4610310c8361af78e
AKB <aravindk797@gmail.com>
2026-07-14 09:57:56 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,18 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        cleaned_list = re.sub('[^A-Za-z0-9]', '', s).lower()
+
+
+        l_ptr = 0
+        r_ptr = len(cleaned_list)-1
+
+        while(l_ptr < r_ptr):
+            if(cleaned_list[l_ptr] != cleaned_list[r_ptr]):
+                return False
+            l_ptr += 1
+            r_ptr -= 1
+
+        return True
