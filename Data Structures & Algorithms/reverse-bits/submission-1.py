a612c7fae3532868380e5603477cf9e8345db9d7
AKB <aravindk797@gmail.com>
2026-07-21 21:38:41 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    
+    def reverseBits(self, n: int) -> int:
+        
+        bitIndex = 0
+        maxBits = 32
+        newNumb = 0
+
+        while(bitIndex < maxBits):
+            bit_mask = 1 << bitIndex
+            neg_bit_mask = 1 << (31 - bitIndex)
+            currBit = bit_mask & n
+
+            if(currBit):
+                newNumb = newNumb | neg_bit_mask
+                print ("newNumb",f"{newNumb:032b}")
+            
+            bitIndex += 1
+
+        return newNumb
