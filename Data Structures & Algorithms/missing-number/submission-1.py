de8586c06790ed430042a9d0cafe6819aaeb10e4
AKB <aravindk797@gmail.com>
2026-07-22 10:01:25 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.py
@@ -0,0 +1,18 @@
+import math
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        '''
+        1. a= sum to n natural numbers_ n*(n+1)/2
+        2. b= sum of existing elem
+        3. missing num = a-b
+        '''
+
+        n = len(nums)
+        a = (n*(n+1))/2
+        b = sum(nums)
+        mx = max(nums)
+
+        missing = abs(b-a)
+        return int(missing)
